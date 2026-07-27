bf91fcca89e7c80fde18542950251827f5305d3f
jjn <jinx@local.dev>
2026-07-27 17:18:27 -0400

    Rename flake host configuration

--- flake.nix
+++ flake.nix
@@ -1,27 +1,32 @@
 {
-  description = "my nixOS config";
+  description = "Jinx's NixOS configuration";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+
     home-manager = {
       url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }: {
+  outputs = { nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
-      nixos-btw = nixpkgs.lib.nixosSystem {
+      desktop-nvidia = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
 
         modules = [
           ./hosts/desktop-nvidia
+
           home-manager.nixosModules.home-manager
+
           {
             home-manager = {
               useGlobalPkgs = true;
               useUserPackages = true;
-              users."jinx" = import ./home/default.nix;
+
+              users.jinx = import ./home;
+
               backupFileExtension = "backup";
             };
           }
