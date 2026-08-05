0feae7889c92f9fede4bb8f468f166e6d63361bb
jjn <jinx@local.dev>
2026-08-04 20:52:53 -0400

    Add /home on second NVMe (btrfs) and switch to zram swap

--- modules/hardware/default.nix
+++ modules/hardware/default.nix
@@ -2,5 +2,6 @@
   imports = [
     ./nvidia.nix
     ./graphics.nix
+    ./swap.nix
   ];
 }
---
+++ modules/hardware/swap.nix
@@ -0,0 +1,6 @@
+{
+  zramSwap = {
+    enable = true;
+    memoryPercent = 50;  # ~15G compressed swap, given 31G RAM
+  };
+}
