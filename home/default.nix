df36a23c8d95eaf798d594ce812a522716726379
jjn <jinx@local.dev>
2026-08-04 21:08:38 -0400

    Add direnv with nix-direnv and zsh integration

--- home/default.nix
+++ home/default.nix
@@ -1,19 +1,16 @@
 { config, pkgs, ... }:
-
 {
   imports = [
     ./programs/git.nix
     ./programs/foot.nix
     ./programs/zsh.nix
-
+    ./programs/direnv.nix
     ./desktop/dconf.nix
     ./desktop/portals.nix
     ./desktop/dotfiles.nix
   ];
-
   home.username = "jinx";
   home.homeDirectory = "/home/jinx";
   home.stateVersion = "26.05";
-
   home.packages = import ./packages.nix { inherit pkgs; };
 }
