b190506781c847c9549fc720cd8bcec27f564899
jjn <jinx@local.dev>
2026-08-12 21:38:57 -0400

    Add automatic weekly nix-gc and nix-optimise

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -1,8 +1,18 @@
 {
   nixpkgs.config.allowUnfree = true;
-
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
   ];
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 14d";
+  };
+
+  nix.optimise = {
+    automatic = true;
+    dates = [ "weekly" ];
+  };
 }
