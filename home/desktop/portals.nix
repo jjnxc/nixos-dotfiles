e4ff3e25047ce8d8806d669b71a45e9ef371f347
jjn <jinx@local.dev>
2026-08-18 21:17:10 -0400

    hypr/boot: dedupe portal registration, dedupe nvidia env vars, cap boot entries

--- home/desktop/portals.nix
+++ home/desktop/portals.nix
@@ -3,7 +3,6 @@
   xdg.portal = {
     enable = true;
     extraPortals = [
-      pkgs.xdg-desktop-portal-hyprland
       pkgs.xdg-desktop-portal-gtk
     ];
     config = {
--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -1,8 +1,3 @@
 {
   programs.hyprland.enable = true;
-
-  environment.sessionVariables = {
-    WLR_NO_HARDWARE_CURSORS = "1";
-    LIBVA_DRIVER_NAME = "nvidia";
-  };
 }
