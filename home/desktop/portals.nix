f1d171d0369c805712a1f135a7a776d6adb7a3ab
jjn <jinx@local.dev>
2026-09-09 11:56:47 -0400

    fix: configure Hyprland portals for screen recording

--- home/desktop/portals.nix
+++ home/desktop/portals.nix
@@ -7,6 +7,9 @@
     ];
     config = {
       common.default = [ "hyprland" "gtk" ];
+      "org.freedesktop.impl.portal.ScreenCast".default = [ "hyprland" ];
+      "org.freedesktop.impl.portal.Screenshot".default = [ "hyprland" ];
+      "org.freedesktop.impl.portal.GlobalShortcuts".default = [ "hyprland" ];
     };
   };
 }
